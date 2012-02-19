09b5bbe33dac4933779bc772e95d1bbe2afd7b52
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2012-02-19 20:09:13 +0900

    initial import

---
+++ cookbooks/bark/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: ./
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+cookbook_file "/etc/httpd/conf.d/bark.conf" do
+  source "bark.conf"
+  mode "0644"
+end
+
+script "install_bark" do
+  interpreter "bash"
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    rsync -avz /vagrant /var/www/html/bark/
+    chown apache.apache -R /var/www/html/bark
+    chmod 777 -R /var/www/html/bark
+    /etc/init.d/httpd restart
+  EOH
+end
